571f11ed693160e0be248917e8d81ae51d42e9f9
Alex Pécsi <alex.pecsi@gmail.com>
2015-09-30 16:41:28 +0100

    Fixed assembly plugin

--- vaultfx/src/main/java/so/blacklight/vault/fx/VaultController.java
+++ vaultfx/src/main/java/so/blacklight/vault/fx/VaultController.java
@@ -1,6 +1,18 @@
 package so.blacklight.vault.fx;
 
+import javafx.event.ActionEvent;
+
 public class VaultController {
 
+    public void addVaultEntry(final ActionEvent event) {
 
+    }
+
+    public void searchText(final ActionEvent event) {
+
+    }
+
+    public void removeVaultEntry(final ActionEvent event) {
+
+    }
 }
