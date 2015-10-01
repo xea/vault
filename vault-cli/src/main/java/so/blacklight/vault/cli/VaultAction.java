fa84dc6dabf247dfafeb02f0f0df2a58da29c5c6
xea <alex.pecsi@gmail.com>
2015-10-01 21:30:33 +0100

    update :/

---
+++ vault-cli/src/main/java/so/blacklight/vault/cli/VaultAction.java
@@ -0,0 +1,64 @@
+package so.blacklight.vault.cli;
+
+/**
+ * Actions a user could possibly request
+ */
+public enum VaultAction {
+
+    /**
+     * Do nothing
+     */
+    NONE("none"),
+
+    /**
+     * Print usage to the screen
+     */
+    SHOW_USAGE("help"),
+
+    /**
+     * Create a new vault from scratch
+     */
+    CREATE_VAULT("create-vault"),
+
+    /**
+     * Create a new folder within the selected vault
+     */
+    CREATE_FOLDER("create-folder"),
+
+    /**
+     * Delete the specified folder from the selected vault
+     */
+    DELETE_FOLDER("delete-folder"),
+
+    /**
+     * Create a new vault entry
+     */
+    CREATE_ENTRY("create-entry"),
+
+    /**
+     * Delete the selected vault entry
+     */
+    DELETE_ENTRY("delete-entry"),
+
+    /**
+     * Display entries from the specified folder
+     */
+    SHOW_FOLDERS("list-folder"),
+
+    /**
+     * Copy the contents of a secret entry to the system clipboard
+     */
+    COPY_SECRET("copy-secret");
+
+
+    private String actionCode;
+
+    VaultAction(String code) {
+        actionCode = code;
+    }
+
+    public String getActionCode() {
+        return actionCode;
+    }
+
+}
--- vault-cli/src/main/java/so/blacklight/vault/cli/VaultCLI.java
+++ vault-cli/src/main/java/so/blacklight/vault/cli/VaultCLI.java
@@ -5,19 +5,92 @@ import com.github.jankroken.commandline.OptionStyle;
 import so.blacklight.vault.Credentials;
 import so.blacklight.vault.Vault;
 import so.blacklight.vault.VaultStore;
-import so.blacklight.vault.entry.PasswordEntry;
 
-import java.io.*;
+import java.io.Console;
+import java.io.File;
+import java.io.IOException;
 
+/**
+ * Provides a command line utility for accessing secure vaults.
+ */
 public class VaultCLI {
 
     public static final String DEFAULT_VAULT_PATH = "vault.vlt";
 
-    private final CommandLineOptions opts;
+    public static void main(final String[] args) throws Exception {
+        final CLIOptions cliOpts = CommandLineParser.parse(CLIOptions.class, args, OptionStyle.SIMPLE);
+        final VaultCLI cli = new VaultCLI();
+
+        cli.processRequest(cliOpts);
+    }
+
+    /**
+     * Attempt to execute user request
+     * @param opts command line options
+     */
+    public void processRequest(final CLIOptions opts) throws IOException {
+        switch (opts.getAction()) {
+            case CREATE_VAULT:
+                createVault(opts);
+                break;
+            default:
+                showUsage();
+                break;
+        }
+    }
+
+    /**
+     * Creates a new, empty vault.
+     * @param opts
+     */
+    protected void createVault(final CLIOptions opts) throws IOException {
+        final File vaultFile = new File(opts.getVaultPath());
+
+        if (vaultFile.exists()) {
+            System.out.println("ERROR: File " + vaultFile.getAbsolutePath() + " already exists");
+        } else {
+            final Console console = System.console();
+
+            if (console == null) {
+                System.out.println("ERROR: Could not open console");
+            } else {
+                System.out.print("Enter vault password: ");
+                final String password1 = String.valueOf(console.readPassword());
+                System.out.print("Enter vault password again: ");
+                final String password2 = String.valueOf(console.readPassword());
+
+                if (password1.equals(password2)) {
+                    System.out.print("Enter one-time password: ");
+                    final char[] otp = System.console().readPassword();
+
+                    final Vault vault = new Vault();
+                    final Credentials credentials = new Credentials(String.valueOf(password1), String.valueOf(otp));
+
+                    vault.lock(credentials);
+                    new VaultStore().save(vault, vaultFile);
+                } else {
+                    System.out.println("ERROR: Passwords are different");
+                }
+            }
+        }
+        //final StringSelection ss = new StringSelection("valami");
+        //Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, ss);
+    }
+
+    /**
+     * Display a helpful message explaining how to use this program to the screen
+     */
+    protected void showUsage() {
+        System.out.println("Usage: ");
+        // TODO add extremely useful message here
+    }
+
+    /**
+    private final CLIOptions opts;
 
     private final Vault vault;
 
-    public VaultCLI(final CommandLineOptions opts) throws IOException, ClassNotFoundException {
+    public VaultCLI(final CLIOptions opts) throws IOException, ClassNotFoundException {
         this.opts = opts;
 
         final File vaultFile = new File(opts.getVaultPath().orElse(DEFAULT_VAULT_PATH));
@@ -69,12 +142,12 @@ public class VaultCLI {
         });
     }
 
-
     public static final void main(final String[] args) throws Exception {
-        final CommandLineOptions cliOpts = CommandLineParser.parse(CommandLineOptions.class, args, OptionStyle.SIMPLE);
+        final CLIOptions cliOpts = CommandLineParser.parse(CLIOptions.class, args, OptionStyle.SIMPLE);
 
         final VaultCLI cli = new VaultCLI(cliOpts);
         cli.processCommand();
     }
 
+     */
 }
